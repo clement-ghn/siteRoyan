ed5557df4a94aa77a92464f71455615de5c458dc
clément Géhin <clement.gehin@4murs.fr>
2023-06-27 23:17:55 +0200

    lancement du projet de création sous Angular et TypeScript du site des clubs de plage de Royan

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './accueil/accueil.component';
+
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, AccueilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
