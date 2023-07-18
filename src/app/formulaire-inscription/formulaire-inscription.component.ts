8001f8e4497e8d485110b536bd443036c1d0a160
clément Géhin <clement.gehin@4murs.fr>
2023-07-18 22:07:56 +0200

    ajout de la galerie photo + formulaire natation + info rentrées dans pdf

--- src/app/formulaire-inscription/formulaire-inscription.component.ts
+++ src/app/formulaire-inscription/formulaire-inscription.component.ts
@@ -15,11 +15,17 @@ export class FormulaireInscriptionComponent {
   submitForm() {
     this.showThankYouMessage = true;
     const pdf = new jsPDF();
-    const name = this.formData.name;
+    const nom = this.formData.nom;
+    const prenom = this.formData.prenom;
+    const adresse = this.formData.adresse;
+    const adresseRoyan = this.formData.adresseRoyan;
+    const telephone = this.formData.telephone;
     const email = this.formData.email;
-    const hopital = this.formData.hopital;
+    const hopital = this.formData.hopital === 'yes' ? 'Oui' : 'Non';
+    const photos = this.formData.photos === 'yes' ? 'Oui' : 'Non';
+    const sortir = this.formData.sortir === 'yes' ? 'Oui' : 'Non';
 
-    const content = `Nom: ${name}\nEmail: ${email}\n hopital: ${hopital}`;
+    const content = `Nom: ${nom}\nPrénom: ${prenom}\nEmail: ${email}\nAdresse Permanente: ${adresse}\nAdresse sur place: ${adresseRoyan}\nTéléphone: ${telephone} \nSortir seul: ${sortir}\nPrendre les mesures nécessaires d'hospitalisation: ${hopital}\nDroit à l'image: ${photos} `;
     pdf.text(content, 10, 10);
     pdf.save('form_data.pdf');
     }
--- src/app/natation/natation.component.ts
+++ src/app/natation/natation.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { jsPDF } from 'jspdf';
 
 @Component({
-  selector: 'app-natation',
-  template: `
-    <p>
-      natation works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: 'app-formulaire',
+  templateUrl: 'natation.component.html',
+  styleUrls: ['natation.component.css']
 })
 export class NatationComponent {
+  showThankYouMessage: boolean = false;
+  formData: any = {};
 
+  submitForm() {
+    this.showThankYouMessage = true;
+    const pdf = new jsPDF();
+    const nom = this.formData.nom;
+    const prenom = this.formData.prenom;
+    const adresse = this.formData.adresse;
+    const adresseRoyan = this.formData.adresseRoyan;
+    const telephone = this.formData.telephone;
+    const email = this.formData.email;
+    const hopital = this.formData.hopital === 'yes' ? 'Oui' : 'Non';
+    const photos = this.formData.photos === 'yes' ? 'Oui' : 'Non';
+    const sortir = this.formData.sortir === 'yes' ? 'Oui' : 'Non';
+    const horaires = this.formData.horaires;
+
+    const content = `Nom: ${nom}\nPrénom: ${prenom}\nEmail: ${email}\nAdresse Permanente: ${adresse}\nAdresse sur place: ${adresseRoyan}\nTéléphone: ${telephone} \nHoraires des cours: ${horaires} \nSortir seul: ${sortir}\nPrendre les mesures nécessaires d'hospitalisation: ${hopital}\nDroit à l'image: ${photos} `;
+    pdf.text(content, 10, 10);
+    pdf.save('form_data.pdf');
+    }
 }
+
